2a0814fe42e7cf2d299b4ba50aafde8ec1fc42c5
Sennthx <totharon1173@gmail.com>
2022-12-20 22:46:57 +0100

    added show weather functionality, showError, and formReset

--- scripts/ui.js
+++ scripts/ui.js
@@ -2,8 +2,20 @@ class UI {
     constructor() {
         this.timeOutID = 0;
     }
-    showWeatherData(weatherData) {
-
+    showWeatherData(data) {
+        // Getting elements and inserting data
+        document.querySelector("#location-name").innerText = `${data.name}, ${data.sys.country}`;
+        document.querySelector("#description").innerText = `${data.weather[0].description}`;
+        document.querySelector("#temperature").innerText = `${data.main.temp.toFixed(1)} C°`;
+        document.querySelector("#humidity").innerText = `Relative Humidity: ${data.main.humidity}`;
+        document.querySelector("#feels-like").innerText = `Feels like: ${data.main.feels_like}`;
+        document.querySelector("#wind").innerText = `Wind direction: ${data.wind.deg}`;
+        document.querySelector("#wind").innerText = `Wind Speed: ${data.wind.speed.toFixed(1)} m/s`;
+        document.querySelector("#longitude").innerText = `Longitude: ${data.coord.lon.toFixed(3)}`;
+        document.querySelector("#latitude").innerText = `Longitude: ${data.coord.lat.toFixed(3)}`;
+        console.log(document.querySelector("#details").lastChild);
+        //document.querySelector("#humidity").innerText = `${data.weather[0].icon}`;
+        console.log(data);
     }
 
     showError(message) {
@@ -13,7 +25,7 @@ class UI {
         box.style.display = "block";
         this.timeOutID = setTimeout(() => {
             box.style.display = "none";
-        }, 3000);
+        }, 5000);
     }
 
     resetForm() {
